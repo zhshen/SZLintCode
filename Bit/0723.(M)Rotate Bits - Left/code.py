ebeccbdf76bd0aa224486d865243c2c524522613
Zheng Shen <shenzheng72@hotmail.com>
2020-07-25 01:19:54 +0800

    Add 0723

---
+++ Bit/0723.(M)Rotate Bits - Left/code.py
@@ -0,0 +1,10 @@
+class Solution:
+    """
+    @param n: a number
+    @param d: digit needed to be rorated
+    @return: a number
+    """
+    def leftRotate(self, n, d):
+        return 0
+
+print(Solution().leftRotate(1, 2))
